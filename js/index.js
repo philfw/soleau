6ba87f81a42012117e0cd50f41f7083fa3f24109
philfw <philfw@gmail.com>
2016-02-23 12:46:11 -0500

    first draft done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 
-$('.next').on('click', function (){
+$('.next').on('click', function (event){
+	event.preventDefault();
 	$('html, body').animate({
 		scrollTop: $( $( this ).attr('href') ).offset().top
 	}, 500);
@@ -7,10 +8,19 @@ $('.next').on('click', function (){
 
 $('form#publish').on('submit', function (event){
 	event.preventDefault();
+	$('input#email').attr('value','');
 	$('form').slideUp(500, function() {
+		$('#seal').fadeIn(800);
 		$('.complete').slideDown(500)
 	});
+	window.open('https://twitter.com/share?text=I just registered a work with %23Soleau IDno000000');
 });
 
+$('#seal').on('click', function (){
+	$('.complete').toggle();
+	$('#seal').fadeOut(300, function() {
+		$('form#publish').slideDown(500);
+	});
+});
 
 
